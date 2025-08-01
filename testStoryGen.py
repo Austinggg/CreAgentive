a28b50db192f85daf3472d34c75ba6c192d59367
Austin <1332628018@qq.com>
2025-08-01 18:19:22 +0800

    update StoryGen

--- testStoryGen.py
+++ testStoryGen.py
@@ -4,7 +4,7 @@ from Resource.llmclient import LLMClientManager
 
 # 初始化模型客户端
 model_client = LLMClientManager().get_client("pro-deepseek-v3")
-storygenworkflow = StoryGenWorkflow(model_client, maxround=3) # 故事生成工作流
+storygenworkflow = StoryGenWorkflow(model_client, maxround=1) # 故事生成工作流
 
 # 测试运行工作流
 asyncio.run(storygenworkflow.run())
