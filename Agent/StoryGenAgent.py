0d5dbf3fd2d8727b946f19b4b9a14e7131915cea
Austin <1332628018@qq.com>
2025-08-13 22:48:41 +0800

    update StoryGenAgent

--- Agent/StoryGenAgent.py
+++ Agent/StoryGenAgent.py
@@ -1,6 +1,6 @@
 from autogen_agentchat.agents import AssistantAgent
 from Resource.template.storygen_prompt.longgoal import longgoal_prompt_template
-from Resource.template.storygen_prompt.shortgoal import shortgoal_prompt_template
+from Resource.template.storygen_prompt.shortgoal import SHORTGOAL_AGENT_PROMPT_TEMPLATE
 from Resource.template.storygen_prompt.decision import decision_prompt_template
 
 
@@ -9,7 +9,7 @@ def create_agents(model_client):
         name="shortgoal_agent",
         description="生成短期目标，即当前任务的目标",
         model_client=model_client,
-        system_message=shortgoal_prompt_template
+        system_message=SHORTGOAL_AGENT_PROMPT_TEMPLATE
     )
 
     longgoal_agent = AssistantAgent(
